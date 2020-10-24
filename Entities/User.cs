8ab8d4a67c0291a04a69e8074fb5857ea44e9cd5
Mária Takács <takacsmaria5858@gmail.com>
2020-10-24 17:46:39 +0200

    Initial commit

---
+++ Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public class User
+    {
+      
+            public Guid ID { get; set; } = Guid.NewGuid();
+            public string FullName { get; set; }
+
+    }
+}
+}
