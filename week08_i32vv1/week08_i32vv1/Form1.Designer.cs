6a9d4c5228268ecbb5fb6a7d8864aa31cde74173
Mária Takács <takacsmaria5858@gmail.com>
2020-11-15 20:38:28 +0100

    commit

--- week08_i32vv1/week08_i32vv1/Form1.Designer.cs
+++ week08_i32vv1/week08_i32vv1/Form1.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "BALL";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(231, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "PRESENT";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(939, 668);
+            this.ClientSize = new System.Drawing.Size(599, 394);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- week08_i32vv1/week08_i32vv1/Form1.cs
+++ week08_i32vv1/week08_i32vv1/Form1.cs
@@ -12,9 +12,121 @@ namespace week08_i32vv1
 {
     public partial class Form1 : Form
     {
+        private Toy _nextToy;
+    
+        private List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
+        public IToyFactory Factory
+        {
+            get { return _factory; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var toy in _toys)
+            {
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _toys[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory()
+            {
+                BallColor = button.BackColor
+            };
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 50;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorpicker = new ColorDialog();
+
+            colorpicker.Color = button.BackColor;
+            if (colorpicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorpicker.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Factory = new PresentFactory()
+            {
+                BoxColor = button4.BackColor,
+                RibbonColor = button5.BackColor
+            };
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorpicker = new ColorDialog();
+
+            colorpicker.Color = button.BackColor;
+            if (colorpicker.ShowDialog() != DialogResult.OK)
+                return;
+            button5.BackColor = colorpicker.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorpicker = new ColorDialog();
+
+            colorpicker.Color = button.BackColor;
+            if (colorpicker.ShowDialog() != DialogResult.OK)
+                return;
+            button4.BackColor = colorpicker.Color;
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
