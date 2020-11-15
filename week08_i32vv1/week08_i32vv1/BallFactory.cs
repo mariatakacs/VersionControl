c862896be8fa0bc41b24c53011e9f65b2a7114bb
Mária Takács <takacsmaria5858@gmail.com>
2020-11-15 21:26:59 +0100

    commit

--- week08_i32vv1/week08_i32vv1/BallFactory.cs
+++ week08_i32vv1/week08_i32vv1/BallFactory.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using week08_i32vv1.Abstractions;
 
 namespace week08_i32vv1
 {
-   public class BallFactory
+   public class BallFactory : IToyFactory
     {
         public Ball CreateNew()
         {
