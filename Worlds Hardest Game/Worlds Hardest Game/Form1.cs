b52d8fb8b9b4be2ec768c8d72da2afd578e747b9
Mária Takács <takacsmaria5858@gmail.com>
2020-11-29 22:23:09 +0100

    commit

--- Worlds Hardest Game/Worlds Hardest Game/Form1.cs
+++ Worlds Hardest Game/Worlds Hardest Game/Form1.cs
@@ -15,6 +15,26 @@ namespace Worlds_Hardest_Game
     {
         GameController gc = new GameController();
         GameArea ga;
+        Brain winnerBrain = null;
+
+        // ...
+
+        private void Gc_GameOver(object sender)
+        {
+            // ...
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            // ...
+        }
 
         public Form1()
         {
@@ -38,12 +58,32 @@ namespace Worlds_Hardest_Game
             }
             gc.Start();
 
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
             private void Gc_GameOver(object sender)
             {
                 generation++;
                 label1.Text = string.Format(
                     "{0}. generáció",
                     generation);
+                gc.ResetCurrentLevel();
+                foreach (var p in topPerformers)
+                {
+                    var b = p.Brain.Clone();
+                    if (generation % 3 == 0)
+                        gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                    else
+                        gc.AddPlayer(b);
+
+                    if (generation % 3 == 0)
+                        gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                    else
+                        gc.AddPlayer(b.Mutate());
+                }
+                gc.Start();
             }
 
         }
@@ -51,6 +91,15 @@ namespace Worlds_Hardest_Game
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
